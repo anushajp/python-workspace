910ac62b42f79db9782fbc9336e183120f1a3719
anusha <anushaprasanth10@gmail.com>
2017-09-30 13:33:10 +0530

    learning python reduce function

---
+++ python_reduce.py
@@ -0,0 +1,8 @@
+"""
+The function reduce(func, seq) continually applies the function func() to the sequence seq.
+It returns a single value.
+"""
+list1 = [1, 2, 3, 4, 5]
+print(reduce(lambda x, y: x+y, list1))
+
+
